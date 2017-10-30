a30c9b65fc24a792bb2e48935f038ff15d564781
Puzhen Qian <puqian@ebay.com>
2017-10-30 16:27:25 +0800

    Add a put get test

---
+++ Idea-Project/src/main/java/com/puzhen/visionstorage/script/PutGetTest.java
@@ -0,0 +1,40 @@
+package com.puzhen.visionstorage.script;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PutGetTest {
+
+    @Test
+    public void test0() {
+        System.out.println("Put and get from a HBASE table..");
+        Configuration config = HBaseConfiguration.create();
+        config.addResource(new Path("hbase-site.xml"));
+        try {
+            HBaseAdmin.checkHBaseAvailable(config);
+            Connection connection = ConnectionFactory.createConnection(config);
+            Table table = connection.getTable(tableTen);
+            Put p = new Put(Bytes.toBytes("key1"));
+            p.addImmutable(family, qualifier, Bytes.toBytes("value1"));
+            table.put(p);
+
+            Get get = new Get(Bytes.toBytes("key1"));
+            Result r = table.get(get);
+            byte[] value = r.getValue(family, qualifier);
+            assertEquals("value1", Bytes.toString(value));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static TableName tableTen = TableName.valueOf("table1");
+    private static byte[] family = Bytes.toBytes("family");
+    private static byte[] qualifier = Bytes.toBytes("qualifier");
+}
